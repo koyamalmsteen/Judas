101668d6e90a052ea6ac90e0063c60bccc0aebcf
Yukinobu KOYAMA <ykoyama@kugi.kyoto-u.ac.jp>
2013-07-11 04:21:35 +0900

    Fixed

--- workspace/Plugins/bridges/JRuby/plotDstIndex.rb
+++ workspace/Plugins/bridges/JRuby/plotDstIndex.rb
@@ -5,15 +5,15 @@ require "/home/iugonet/Judas/lib/external/jcommon-1.0.17.jar"
 require "/home/iugonet/Judas/lib/Aplot.jar"
 require "/home/iugonet/Judas/lib/Tplot.jar"
 require "/home/iugonet/Judas/plugins/DstIndex.jar"
+java_import org.jfree.chart.ChartFactory
 java_import java.lang.String
-
-include_class Java::org.iugonet.www.DstIndex
+java_import org.iugonet.www.DstIndex
 
 strUrl="http://wdc-data.iugonet.org/data/hour/index/dst/1984/dst8410"
-#dstIndex=DstIndex.DstIndex()
-timeSeriesCollection=DstIndex.loadData(strUrl)
+dstIndex=Java::dstindex.dstindex()
+#timeSeriesCollection=Java::dstIndex.loaddata(strUrl)
 
-chart=jfree.chart.ChartFactory.createTimeSeriesChart(lang.String("Dst Index"),lang.String("Time"),lang.String("Dst Index [nT]"),timeSeriesCollection,lang.Boolean("FALSE"),lang.Boolean("FALSE"),lang.Boolean("TRUE"))
-frame=jfree.chart.ChartFrame(lang.String("Simple TimeSeries Chart"),chart)
+chart=Java::ChartFactory.createTimeSeriesChart(Java::java.lang.String("Dst Index"),Java::java.lang.String("Time"),Java::java.lang.String("Dst Index [nT]"),timeSeriesCollection,Boolean("FALSE"),Java::Boolean("FALSE"),Java::Boolean("TRUE"))
+frame=jfree.chart.ChartFrame(java.lang.String("Simple TimeSeries Chart"),chart)
 frame.pack()
 frame.setVisible=true
