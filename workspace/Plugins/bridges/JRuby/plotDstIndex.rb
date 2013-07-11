3c001e2f551d106e0218c8756d710421f49faa85
Yukinobu KOYAMA <ykoyama@kugi.kyoto-u.ac.jp>
2013-07-11 19:48:48 +0900

    Fixed

--- workspace/Plugins/bridges/JRuby/plotDstIndex.rb
+++ workspace/Plugins/bridges/JRuby/plotDstIndex.rb
@@ -1,10 +1,10 @@
 #!/usr/bin/env jruby
 include Java
-require "/home/iugonet/Judas/lib/external/jfreechart-1.0.14.jar"
-require "/home/iugonet/Judas/lib/external/jcommon-1.0.17.jar"
-require "/home/iugonet/Judas/lib/Aplot.jar"
-require "/home/iugonet/Judas/lib/Tplot.jar"
-require "/home/iugonet/Judas/plugins/DstIndex.jar"
+require_relative "/home/iugonet/Judas/lib/external/jcommon-1.0.17.jar"
+require_relative "/home/iugonet/Judas/lib/external/jfreechart-1.0.14.jar"
+require_relative "/home/iugonet/Judas/lib/Aplot.jar"
+require_relative "/home/iugonet/Judas/lib/Tplot.jar"
+require_relative "/home/iugonet/Judas/plugins/DstIndex.jar"
 java_import javax.swing.JFrame
 java_import org.jfree.chart.ChartFactory
 java_import org.jfree.chart.ChartFrame
@@ -17,6 +17,7 @@ dstIndex=DstIndex.new()
 timeSeriesCollection=dstIndex.loadData(strUrl)
 
 chart=ChartFactory.createTimeSeriesChart(String("Dst Index"),String("Time"),String("Dst Index [nT]"),timeSeriesCollection,FALSE,FALSE,TRUE)
-frame=ChartFrame(String("Simple TimeSeries Chart"),chart)
+#frame=ChartFrame.new_with_sig('Lorg.jfree.chart.ChartFrame',String("Simple TimeSeries Chart"),chart)
+#.java_class.constructor(String("Simple TimeSeries Chart"),null)
 frame.pack()
 frame.setVisible=true
